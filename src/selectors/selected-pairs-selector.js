9ff3384bf7566727467e5697a1306814b35a306b
Michael Bradley <mbradleyis@gmail.com>
2016-08-07 11:29:32 -0700

    defend against undefined exchanges object

--- src/selectors/selected-pairs-selector.js
+++ src/selectors/selected-pairs-selector.js
@@ -1,7 +1,7 @@
 import { createSelector } from 'reselect';
 import selectedExchangeSelector from './selected-exchange-selector'
 
-const getSelectedPairs = selectedExchange => Object.keys(selectedExchange.pairs).map(key => selectedExchange.pairs[key]) || [];
+const getSelectedPairs = selectedExchange => Object.keys(selectedExchange ? selectedExchange.pairs : {}).map(key => selectedExchange.pairs[key]) || [];
 
 export default createSelector(
   selectedExchangeSelector,
