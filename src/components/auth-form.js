b22d6a1db5150e6976a4ce58f3a8aca7207be421
Michael Bradley <mbradleyis@gmail.com>
2016-08-14 16:22:03 -0700

    clear state on logout

--- src/components/auth-form.js
+++ src/components/auth-form.js
@@ -6,7 +6,7 @@ class AuthForm extends Component {
     return <div className="container full-width center-text">
       <input ref="email" type="text" name="email" placeholder="email" />
       <br/>
-      <input ref="password" type="text" name="password" placeholder="password" />
+      <input ref="password" type="password" name="password" placeholder="password" />
       <br/>
       <Button bsStyle="primary" onClick={ () => this.props.loginOrCreateUser({login: true, email: this.refs.email.value, password: this.refs.password.value}) }>Login</Button> <Button bsStyle="primary" onClick={ () => this.props.loginOrCreateUser({login: false, email: this.refs.email.value, password: this.refs.password.value}) }>Sign Up</Button>
     </div>
