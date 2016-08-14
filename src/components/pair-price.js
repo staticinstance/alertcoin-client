1a605c544b3d70c23dd2300b6338ea28edcfbfe0
Michael Bradley <mbradleyis@gmail.com>
2016-08-14 14:55:51 -0700

    increase perf

--- src/components/pair-price.js
+++ src/components/pair-price.js
@@ -3,30 +3,31 @@ import numeral from 'numeral';
 
 class PairPrice extends Component {
     shouldComponentUpdate(nextProps){
-      const answer = this.props && this.props.pair && this.props.pair.status && this.props.pair.key !== nextProps.pair.key && this.props.pair.status !== nextProps.pair.status;
+      const answer = this.props && this.props.pair && this.props.pair.key !== nextProps.pair.key && this.props.pair.status !== nextProps.pair.status;
       return answer;
     }
 
     render() {
-      console.log()
       return <div>
         <div className="main container full-width center-text">
         the price
         </div>
         <div className="container full-width center-text">
           <input type="text" onChange={ this.props.selectPrice } placeholder="Enter a price" value={ this.props.price }/>
-          <div style={{"cursor": "pointer"}} onClick={ () =>
-            this.props.selectPriceClick(numeral(
-              this.props.pair
-                ? this.props.pair.last
-                : 0)
-                .format('0.[00000000]')) }>
-          Last trade was <span className={ this.props.pair && this.props.pair.status ? this.props.pair.status : ""}>{ numeral(
-              this.props.pair
-                ? this.props.pair.last
-                : 0)
-                .format((parseInt(this.props.pair && this.props.pair.last, 10) >= 1) ? '0.[00000000]' : '0.00000000') }</span>
-          </div>
+          {this.props && this.props.pair && this.props.pair.last !== 0
+            ? <div style={{"cursor": "pointer"}} onClick={ () =>
+                  this.props.selectPriceClick(numeral(
+                    this.props.pair
+                      ? this.props.pair.last
+                      : 0)
+                      .format('0.[00000000]')) }>
+                Last trade was <span className={ this.props.pair && this.props.pair.status ? this.props.pair.status : ""}>{ numeral(
+                    this.props.pair
+                      ? this.props.pair.last
+                      : 0)
+                      .format((parseInt(this.props.pair && this.props.pair.last, 10) >= 1) ? '0.[00000000]' : '0.00000000') }</span>
+                </div>
+            : null}
         </div>
     </div>
   }
